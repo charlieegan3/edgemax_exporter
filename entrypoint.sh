af7f84b980d56770ed92ea69ec3fbf672cb28f27
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-04-12 12:02:11 +0100

    Allow a custom env path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -exo pipefail
 
-source /etc/secrets/env
+: "${ENV_PATH:?Need to set ENV_PATH non-empty}"
+
+source $ENV_PATH
 
 : "${EDGEMAX_PASSWORD:?Need to set EDGEMAX_PASSWORD non-empty}"
 
